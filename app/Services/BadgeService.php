cd737214c70734d0eeee314b380da609b178d4f2
Zenn <akanezenn@gmail.com>
2026-06-16 19:13:16 +0700

    Fix: Menambahkan dummy BadgeService untuk mengatasi eror class not found

---
+++ app/Services/BadgeService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class BadgeService
+{
+    /**
+     * Ini adalah class sementara untuk mengatasi error 
+     * karena file aslinya belum di-push oleh tim.
+     */
+    
+    public function __construct()
+    {
+        //
+    }
+
+    // Tambahkan method kosong jika nanti ada error method tidak ditemukan
+}
